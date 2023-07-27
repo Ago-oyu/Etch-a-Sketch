15f709f2a2bff1257d568d0efb745d06440fd658
agoy <yogarz354@gmail.com>
2023-07-27 23:50:42 +0700

    add function to resize canvas

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const div = document.createElement("div");
 const container = document.querySelector('.grid-container');
-
+let currentSize = 16;
 
 div.classList.add("grid");
-for (let i = 0; i < 256; i++) {
-    container.appendChild(div.cloneNode(true));
-}
 
-const grids = document.querySelectorAll(`.grid-container > .grid`)
-console.log(grids)
-grids.forEach(grid => grid.addEventListener("mouseover", function() {
-   hoverGrid(grid); 
-}));
+function setCanvasSize(currentSize) {
+    for (let i = 0; i < currentSize * currentSize; i++) {
+        container.appendChild(div.cloneNode(true));
+    }
+    const grids = document.querySelectorAll(`.grid-container > .grid`)
+    grids.forEach(grid => grid.addEventListener("mouseenter", function() {
+        hoverGrid(grid); 
+    }));
+}
+setCanvasSize(currentSize);
+
+
 
 function hoverGrid(grid) {
     console.log('click');
     grid.classList.add("colored");
 }
+
+function deleteAllGrid() {
+    while (container.firstChild) {
+        container.firstChild.remove();
+    }
+}
+
+function newCanvasSize() {
+    console.log('canvasSize')
+    let newSize = prompt("Canvas Size?");
+    currentSize = newSize;
+    deleteAllGrid();
+    setCanvasSize(currentSize);
+}
+
