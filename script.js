dcfa387273844d6747e2cd41764dc962e8d72978
agoy <yogarz354@gmail.com>
2023-07-27 20:37:57 +0700

    add div as grid

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const div = document.createElement("div");
 const container = document.querySelector('.grid-container');
 
+
 div.classList.add("grid");
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 256; i++) {
     container.appendChild(div.cloneNode(true));
 }
+
+const grid = document.querySelectorAll(`.grid-container > .grid`)
+console.log(grid)
