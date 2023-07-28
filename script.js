6862d4218f61f71d0ec029debc7a878bdfb6b0f3
agoy <yogarz354@gmail.com>
2023-07-28 17:35:26 +0700

    add eraser button

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const div = document.createElement("div");
 const container = document.querySelector('.grid-container');
 let currentSize = 16;
-
-div.classList.add("grid");
+let currentColor = 'grey'
+let eraser = false
 
 function setCanvasSize(currentSize) {
+    setGridSize(currentSize);
     for (let i = 0; i < currentSize * currentSize; i++) {
         container.appendChild(div.cloneNode(true));
     }
+    
     const grids = document.querySelectorAll(`.grid-container > .grid`)
     grids.forEach(grid => grid.addEventListener("mouseenter", function() {
         hoverGrid(grid); 
     }));
 }
-setCanvasSize(currentSize);
-
-
 
 function hoverGrid(grid) {
-    console.log('click');
-    grid.classList.add("colored");
+    if (clickStatus == true) {
+        grid.style.backgroundColor = currentColor;
+    }
+    //grid.classList.add("colored");
 }
 
 function deleteAllGrid() {
@@ -31,8 +32,36 @@ function deleteAllGrid() {
 function newCanvasSize() {
     console.log('canvasSize')
     let newSize = prompt("Canvas Size?");
+    while (newSize > 100) {
+        newSize = prompt("Canvas Size?");
+    }
     currentSize = newSize;
     deleteAllGrid();
     setCanvasSize(currentSize);
 }
 
+function setGridSize(currentSize) {
+    let gridSize = 800/currentSize;
+    div.style.height = `${gridSize}px`;
+    div.style.width = `${gridSize}px`;
+    console.log(div.style.height)
+}
+
+function toogleEraser(){
+    if (eraser == false) {
+        eraser = true;
+        currentColor = 'white'
+    } else {
+        eraser = false;
+        currentColor = 'grey'
+    }
+    
+    console.log(eraser)
+}
+
+let clickStatus = false;
+window.addEventListener('mousedown', function() {clickStatus = true; console.log(clickStatus)});
+window.addEventListener('mouseup', function() {clickStatus = false; console.log(clickStatus)});
+div.classList.add("grid");
+setGridSize(currentSize);
+setCanvasSize(currentSize);
